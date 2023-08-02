2cdb652373e22c7bd4c04e0b6c0e947c03ddb799
Tatjana Dehler <tdehler@suse.com>
2023-08-02 12:12:56 +0200

    Improve help icon zoomed position
    Signed-off-by: Tatjana Dehler <tdehler@suse.com>

--- pkg/rancher-desktop/components/Help.vue
+++ pkg/rancher-desktop/components/Help.vue
@@ -33,46 +33,33 @@ export default Vue.extend({
 
 <template>
   <div class="help-button">
-    <button
+    <i
       v-tooltip="{
         content: tooltip,
         placement: 'left'
       }"
-      class="btn role-fab"
+      class="icon icon-question-mark"
       :class="{
         disabled
       }"
       @click="openUrl"
-    >
-      <span
-        class="icon icon-question-mark"
-        :class="{
-          disabled
-        }"
-      />
-    </button>
+    />
   </div>
 </template>
 
 <style lang="scss" scoped>
 
   .help-button {
-
     .icon {
-      display: inline-block;
+      font-size: 1.3rem;
       background-color: var(--primary);
       color: var(--body-bg);
-      font-size: 1.3rem;
+      border-radius: 50%;
+      text-align: center;
+      padding: 0.085rem;
       width: 1.4rem;
       height: 1.4rem;
-      border-radius: 50%;
       cursor: pointer;
-
-      &:before{
-        padding-top: 5%;
-        padding-left: 2%;
-        display: block;
-      }
 
       &:hover {
         background: var(--primary-hover-bg);
@@ -84,22 +71,6 @@ export default Vue.extend({
       color: var(--body-text);
       opacity: 0.2;
       cursor: default;
-    }
-
-    // We make use of the term Floating Action Button (fab) here because the
-    // design of this button is reminiscent of floating actions buttons from
-    // Material Design
-    .role-fab {
-      all: revert;
-
-      border: 0;
-      padding: 0;
-      background: transparent;
-      transition: background 200ms;
-      border-radius: 50%;
-      &.disabled {
-        border: 0;
-      }
     }
   }
 </style>
