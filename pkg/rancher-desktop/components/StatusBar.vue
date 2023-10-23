55c0c6cb94255bdf4cb5740eba03c084f469eda8
Tatjana Dehler <tdehler@suse.com>
2023-10-23 16:23:56 +0200

    Add status bar component
    Signed-off-by: Tatjana Dehler <tdehler@suse.com>

---
+++ pkg/rancher-desktop/components/StatusBar.vue
@@ -0,0 +1,34 @@
+<script lang="ts">
+import Vue from 'vue';
+
+import BackendProgress from '@pkg/components/BackendProgress.vue';
+
+export default Vue.extend({ components: { BackendProgress } });
+</script>
+
+<template>
+  <footer>
+    <div class="left-column"></div>
+    <div class="right-column">
+      <BackendProgress class="progress" />
+    </div>
+  </footer>
+</template>
+
+<style scoped lang="scss">
+  footer {
+    display: flex;
+    flex-direction: row;
+    padding: 2px 18px 2px 18px;
+    height: var(--footer-height);
+    background-color: var(--footer-bg);
+
+    .left-column {
+      width: 70%;
+    }
+
+    .right-column {
+      width: 30%;
+    }
+  }
+</style>
