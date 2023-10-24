659f6a1bfb2867eed16158c3411f110e56aeb1ef
Tatjana Dehler <tdehler@suse.com>
2023-10-24 18:00:45 +0200

    Add version component
    Signed-off-by: Tatjana Dehler <tdehler@suse.com>

---
+++ pkg/rancher-desktop/components/Version.vue
@@ -0,0 +1,28 @@
+<script lang="ts">
+import Vue from 'vue';
+
+import { ipcRenderer } from '@pkg/utils/ipcRenderer';
+
+export default Vue.extend({
+  data() {
+    return { version: this.t('product.versionChecking') };
+  },
+
+  mounted() {
+    ipcRenderer.on('get-app-version', (event, version) => {
+      this.$data.version = version;
+    });
+    ipcRenderer.send('get-app-version');
+  },
+});
+</script>
+
+<template>
+  <span class="versionInfo">
+    <b v-html="t('product.version')"></b> {{ version }}
+  </span>
+</template>
+
+<style scoped lang="scss">
+
+</style>
